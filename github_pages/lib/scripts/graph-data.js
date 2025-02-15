3b4a2acee3edce0e42f768e9fa009c04c9566756
goopis <111644235+docPoacher@users.noreply.github.com>
2025-02-14 21:54:38 -0500

    rere

---
+++ github_pages/lib/scripts/graph-data.js
@@ -0,0 +1,1 @@
+let graphData={graphOptions:{attractionForce:1,linkLength:10,repulsionForce:150,centralForce:3,edgePruning:100,minNodeRadius:3,maxNodeRadius:7},isInitialized:!0,paths:["create-a-link.html","index.html","sun.html","welcome.html"],nodeCount:4,linkSources:[1,2,3],linkTargets:[2,3,0],labels:["create a link","index","sun","Welcome"],radii:[6.4375,6.4375,7,7],linkCount:3}
